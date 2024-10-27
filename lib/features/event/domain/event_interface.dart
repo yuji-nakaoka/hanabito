008d07021512a0b8dd8d0c815eadc6ada231b223
yuji-nakaoka <yu.creative.131@gmail.com>
2024-10-27 16:51:56 +0900

    update : ファイル名、クラス名の更新

--- lib/features/event/domain/event_interface.dart
+++ lib/features/event/domain/event_interface.dart
@@ -1,7 +1,7 @@
 import 'package:hanabito/features/event/domain/event_model.dart';
 
 /// イベント情報に関するインターフェースを定義
-abstract interface class EventRepository {
+abstract interface class EventInterface {
   /// イベント情報一覧を取得する
   Future<List<EventModel>> fetch();
 }
